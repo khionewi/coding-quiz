859988b8f7f27a00b8c4c36892c879181c386d01
Khionewi <karenfioravanti@gmail.com>
2024-02-28 11:41:51 -0500

    Pcode

---
+++ script.js
@@ -0,0 +1,30 @@
+//User Story
+//AS A coding boot camp student
+//I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+//SO THAT I can gauge my progress compared to my peers
+
+//Acceptance Criteria
+//GIVEN I am taking a code quiz
+//WHEN I click the start button
+//THEN a timer starts and I am presented with a question
+//WHEN I answer a question
+//THEN I am presented with another question
+//WHEN I answer a question incorrectly
+//THEN time is subtracted from the clock
+//WHEN all questions are answered or the timer reaches 0
+//THEN the game is over
+//WHEN the game is over
+//THEN I can save my initials and score
+
+//Pseudocode
+
+//Create a function to initialize the game
+//Create a function to start the game
+//Create a function to start the timer
+//Create a function to display the questions
+//Create a function to display the score
+//Create a function to display the high score
+//Create a function to end the game
+//Create a function to submit the score
+//Create a function to clear the score
+//Create a function to restart the game
